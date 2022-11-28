9d1bcdcaa61cfb058afa6295f18218ff59e868db
Brian Bischoff <bbischoff78@gmail.com>
2022-11-28 11:33:48 -0500

    Initial commit

---
+++ test/f3rva-infrastructure.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as F3RvaInfrastructure from '../lib/f3rva-infrastructure-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/f3rva-infrastructure-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new F3RvaInfrastructure.F3RvaInfrastructureStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
