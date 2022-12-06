3f7d14e47402fd90a4ceba5cf7a45cd154ba3d91
Brian Bischoff <bbischoff78@gmail.com>
2022-12-06 13:28:41 -0500

    removed generated sns code

--- lib/f3rva-stack-storage.ts
+++ lib/f3rva-stack-storage.ts
@@ -2,17 +2,15 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { F3RVAStackProps } from './f3rva-stack-properties';
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class F3RVAStackStorage extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: F3RVAStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // stack paramerters
+    const appName = props!.appName!;
+    const envName = props!.envName!;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'F3RvaBigdataInfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
   }
 }
