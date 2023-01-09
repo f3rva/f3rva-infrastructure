af145654d73ede681380ff1432024f48f8ac96a4
Brian Bischoff <bbischoff78@gmail.com>
2023-01-09 13:34:48 -0500

    added logging and fixed syntax error

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,13 @@
-if [[ ${BRANCH_NAME == ""}]]; then
+if [[ ${BRANCH_NAME} == ""]]; then
   ${BRANCH_NAME}="main";
 fi
 
 # for dev, pull down the latest from main
 GITHUB_REPO=https://github.com/f3rva/f3rva-infrastructure
 SCRIPTS_ROOT=${GITHUB_REPO}/raw/${BRANCH_NAME}/SCRIPTS_ROOT
+
+mkdir -p /app/bootstrap
+cd /app/bootstrap
 
 wget env-${ENV}.sh
 wget setup-core.sh
--- scripts/setup-core.sh
+++ scripts/setup-core.sh
@@ -1,9 +1,10 @@
 #/bin/bash
+LOG=bootstrap.log
 
 yum update -y
 
 # source environment specific variables.  env passed in from user data
-. ./env-${1}.sh
+. ./env-${1}.sh >> ${LOG} 2>&1
 
 # setup application stack
-./setup-httpd.sh
+./setup-httpd.sh  >> ${LOG} 2>&1
