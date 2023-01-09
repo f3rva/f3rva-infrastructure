3fa84a940dff5fc51c6e8d023c730b28c6f3a971
Brian Bischoff <bbischoff78@gmail.com>
2023-01-09 12:28:29 -0500

    changed execute bits

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,11 @@
+if [[ ${BRANCH_NAME == ""}]]; then
+  ${BRANCH_NAME}="main";
+fi
+
+# for dev, pull down the latest from main
+GITHUB_REPO=https://github.com/f3rva/f3rva-infrastructure
+SCRIPTS_ROOT=${GITHUB_REPO}/raw/${BRANCH_NAME}/SCRIPTS_ROOT
+
+wget env-${ENV}.sh
+wget setup-core.sh
+wget setup-httpd.sh
