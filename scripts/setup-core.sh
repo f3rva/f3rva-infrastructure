d2e2e19344d59bb75b8f402f952c9b5bdc501f3c
Brian Bischoff <bbischoff78@gmail.com>
2025-05-15 10:24:04 -0400

    switched to arm version of aws cli

--- scripts/setup-core.sh
+++ scripts/setup-core.sh
@@ -7,7 +7,7 @@ yum update -y
 yum remove awscli
 
 cd /app/bootstrap
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
 
