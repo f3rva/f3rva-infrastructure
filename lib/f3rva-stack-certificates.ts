3787dc2eded735c7284f562c4b4060151e9f6a7d
Brian Bischoff <bbischoff78@gmail.com>
2025-09-19 13:11:54 -0400

    refactored to only use one wildcard cert for multiple domains.  less stuff

--- lib/f3rva-stack-certificates.ts
+++ lib/f3rva-stack-certificates.ts
@@ -13,39 +13,56 @@ export class F3RVAStackCertificates extends cdk.Stack {
     // stack props
     const appName = props!.appName;
     const envName = props!.envName;
+    const baseDomain = props!.baseDomain
     const hostedZoneDomains = props!.dns.hostedZones;
 
+    // create array of subject alternative names based on the hosted zones
+    const sanDomainNames: string[] = [];
+    var primaryHostedZone: route53.IHostedZone = {} as route53.IHostedZone;
+
+    // validationDomains facilitates: acm.CertificateValidation.fromDnsMultiZone
+    const validationDomains = {} as { [key: string]: route53.IHostedZone };
+
+    // look up each hosted zone and prepare the SAN list and validation domains
     hostedZoneDomains.forEach(domain => {
-      ////////////////////////////////////////////////////////////////////////////////////////////////    
-      // lookup the hosted zone
       const hostedZoneName = `${appName}-${envName}-${domain}-hostedZone`;
       const hostedZone = route53.HostedZone.fromLookup(this, hostedZoneName, {
-        domainName: domain, // or the domain name of your hosted zone
+        domainName: domain,
       });
 
-      // create array of subject alternative names based on the hosted zones
-      // each domain gets both the root domain and the www subdomain
-      const sanDomainNames: string[] = [];
-      sanDomainNames.push(`www.${domain}`);
+      // add the naked and wildcard domain to the validation domains
+      validationDomains[domain] = hostedZone;
+      validationDomains["*." + domain] = hostedZone;
+
+      // if it's the primary domain, save the hosted zone
+      // otherwise add the wildcard to the SAN list, we don't need to add the 
+      // wildcard for the primary domain because it's specified as the main domain name
+      // on the certificate
+      if (domain === baseDomain) {
+        primaryHostedZone = hostedZone;
+      } else {
+        sanDomainNames.push("*." + domain);
+      }
+      // always add the naked domain
       sanDomainNames.push(domain);
+    });
 
-      ////////////////////////////////////////////////////////////////////////////////////////////////
-      // wildcard certificate
-      const wildcardDomainName = "*." + domain;
-      const wildcardCertName = `${appName}-${envName}-${domain}-wildcardCert`;
-      const wildcardCertificate = new cm.Certificate(this, wildcardCertName, {
-        domainName: wildcardDomainName,
-        subjectAlternativeNames: sanDomainNames,
-        certificateName: `${appName}-${envName}-${domain}-wildcardCert`,
-        validation: cm.CertificateValidation.fromDns(hostedZone)
-      });
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // wildcard certificate
+    const wildcardDomainName = "*." + primaryHostedZone.zoneName;
+    const wildcardCertName = `${appName}-${envName}-wildcardCert`;
+    const wildcardCertificate = new cm.Certificate(this, wildcardCertName, {
+      domainName: wildcardDomainName,
+      subjectAlternativeNames: sanDomainNames,
+      certificateName: `${appName}-${envName}-wildcardCert`,
+      validation: cm.CertificateValidation.fromDnsMultiZone(validationDomains)
+    });
 
-      ////////////////////////////////////////////////////////////////////////////////////////////////
-      // outputs
-      new cdk.CfnOutput(this, `${domain}-WildcardCertificateArn`, {
-        value: wildcardCertificate.certificateArn,
-        exportName: `${appName}-${envName}-${domain.replace(/\./g, "-")}-wildcardCertificateArn`
-      });
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // outputs
+    new cdk.CfnOutput(this, `WildcardCertificateArn`, {
+      value: wildcardCertificate.certificateArn,
+      exportName: `${appName}-${envName}-wildcardCertificateArn`
     });
     
     ////////////////////////////////////////////////////////////////////////////////////////////////
